b1a22c0a59570a1df5617c5c7a37eff926c2b09d
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2017-07-08 17:02:29 +0200

    C++ AI Behaviour Tree Tasks pt2

--- Source/FPS/ChooseNextWaypoint.cpp
+++ Source/FPS/ChooseNextWaypoint.cpp
@@ -1,10 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "Classes/AIController.h"
+
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	UE_LOG(LogTemp, Warning, TEXT("AI in C++"))
+	//Get the Controled Pawn
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	APawn* ControlledPawn = AIController->GetPawn();
+	//Get Patrolling points
+	APatrollingGuard* PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	TArray<AActor*> PatrolPointsCPP = PatrollingGuard->PatrolPointsCPP;
+	//Set Next Waypoint
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	int32 Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPointsCPP[Index]); //Should be protected if empty!!
+	//CycleIndexes
+	int32 NextIndex = (Index + 1) % PatrolPointsCPP.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
+	//UE_LOG(LogTemp, Warning, TEXT("%d"), Index)
 	return EBTNodeResult::Succeeded;
 }
 
--- Source/FPS/ChooseNextWaypoint.h
+++ Source/FPS/ChooseNextWaypoint.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTTaskNode.h"
+#include "PatrollingGuard.h"
 #include "ChooseNextWaypoint.generated.h"
 
 /**
@@ -15,6 +16,11 @@ class FPS_API UChooseNextWaypoint : public UBTTaskNode
 	GENERATED_BODY()
 	
 		virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-	
+protected:
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+		struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = Blackboard)
+		struct FBlackboardKeySelector WaypointKey;
 	
 };
