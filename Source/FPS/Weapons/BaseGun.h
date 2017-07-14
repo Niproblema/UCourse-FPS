7f565f47806886be2b17a24e2e53631e22e17408
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2017-07-14 10:00:22 +0200

    Commit before gun refactoring

---
+++ Source/FPS/Weapons/BaseGun.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseGun.generated.h"
+
+UCLASS()
+class FPS_API ABaseGun : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseGun();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	void OnFire();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
