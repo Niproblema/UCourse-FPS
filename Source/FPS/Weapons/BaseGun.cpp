8a5853295e0d23a6415dc9a1ce9297f91e16c516
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2017-07-14 13:00:22 +0200

    Gun Actor added and c++ methods

--- Source/FPS/Weapons/BaseGun.cpp
+++ Source/FPS/Weapons/BaseGun.cpp
@@ -1,31 +1,79 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BaseGun.h"
+#include "Weapons/BaseProjectile.h"
+#include "Animation/AnimInstance.h"
+#include "Kismet/HeadMountedDisplayFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "MotionControllerComponent.h"
 
 
 // Sets default values
-ABaseGun::ABaseGun()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ABaseGun::ABaseGun() {
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	// Create a gun mesh component
+	GunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Gun"));
+	GunMesh->SetOnlyOwnerSee(true);			// only the owning player will see this mesh
+	GunMesh->bCastDynamicShadow = false;
+	GunMesh->CastShadow = false;
+	// FP_Gun->SetupAttachment(Mesh1P, TEXT("GripPoint"));
+	GunMesh->SetupAttachment(RootComponent);
+
+	MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
+	MuzzleLocation->SetupAttachment(GunMesh);
+	MuzzleLocation->SetRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
 
 }
 
 // Called when the game starts or when spawned
-void ABaseGun::BeginPlay()
-{
+void ABaseGun::BeginPlay() {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
-void ABaseGun::Tick(float DeltaTime)
-{
+void ABaseGun::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 
 }
 
 
 void ABaseGun::OnFire() {
+	// try and fire a projectile
+	if (ProjectileClass != NULL)
+	{
+		UWorld* const World = GetWorld();
+		if (World != NULL)
+		{
+			const FRotator SpawnRotation = MuzzleLocation->GetComponentRotation();
+			// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+			const FVector SpawnLocation = MuzzleLocation->GetComponentLocation();
+
+			//Set Spawn Collision Handling Override
+			FActorSpawnParameters ActorSpawnParams;
+			ActorSpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
+
+			// spawn the projectile at the muzzle
+			World->SpawnActor<ABaseProjectile>(ProjectileClass, SpawnLocation, SpawnRotation, ActorSpawnParams);
+		}
+	}
+
+	// try and play the sound if specified
+	if (FireSound != NULL)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+	}
+
+	// try and play a firing animation if specified
+	if (FireAnimation != NULL)
+	{
+		// Get the animation object for the arms mesh
+		if (AnimInstance != NULL)
+		{
+			AnimInstance->Montage_Play(FireAnimation, 1.f);
+		}
+	}
 }
 
--- Source/FPS/Weapons/BaseGun.h
+++ Source/FPS/Weapons/BaseGun.h
@@ -10,20 +10,45 @@ UCLASS()
 class FPS_API ABaseGun : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* GunMesh;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USceneComponent* MuzzleLocation;
+
+public:
 	// Sets default values for this actor's properties
 	ABaseGun();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+		TSubclassOf<class ABaseProjectile> ProjectileClass;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		class USoundBase* FireSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		class UAnimMontage* FireAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+		class UAnimInstance* AnimInstance;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	void OnFire();
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
 
-	
-	
+
+
+
 };
